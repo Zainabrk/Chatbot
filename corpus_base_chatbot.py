bdaa0a587f0610d1d2cdc638d60662ffb08b050e
mosamarafique <i180639@nu.edu.pk>
2023-01-26 19:28:49 +0500

    Additional Questions

--- corpus_base_chatbot.py
+++ corpus_base_chatbot.py
@@ -4,6 +4,14 @@ import nltk
 from nltk.corpus import stopwords
 from nltk import ne_chunk, pos_tag, word_tokenize
 from nltk.tree import Tree
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('averaged_perceptron_tagger')
@@ -12,6 +20,7 @@ nltk.download('words')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 import PyPDF2
+import pycountry
 import os
 import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
@@ -42,16 +51,31 @@ def summary(sample):
   return text_summary
 
 def abstract(text):
-  idx1 = text.index("abstract")
-  idx2 = text.index("keywords") 
+  if ("abstract" in text):
+    idx1 = text.index("abstract")
+    if ("keywords" in text):
+      idx2 = text.index("keywords") 
+    else:
+      if "introduction" in text:
+        idx2 = text.index("introduction") 
+      else:
+        idx2 = idx1+200
+  else:
+    if "introduction" not in text:
+      return "Not Found"
+    idx1 = text.index("introduction")
+    idx2 = idx1+200
+
   abstract = ''
   for idx in range(idx1 + len("abstract") + 1, idx2):
     abstract = abstract + text[idx]
   return abstract
 
 def talkabout(text):
+  notneeded=["scope","research","outlook","techniques","technique","directions","direction","location" ,"author","talks","talk","whose","abstract","summary","talking","problem","statement","tools","tool","technologies","technology","approaches","approach","future","work","works"]
   data = text.translate(str.maketrans('', '', string.punctuation))
   data = data.split() 
+  data = list(set(data) - set(notneeded))
   data = [singularize(plural) for plural in data]
   stop_words = set(stopwords.words('english'))
   filtered_sentence = []
@@ -93,10 +117,179 @@ def author(text):
       values+=(value.pop())
       values+=", "
       count+=1
-    return "The author of the paper are "+values
+    return values
   else:
     value = list(Author.keys())
-    return "The author of the paper is "+value.pop()
+    if len(value)>0:
+      return value.pop()
+    else:
+      return "Not Found"
+
+def authorlocation(text):
+  idx1 = 0
+  if "Abstract" in text:
+    idx1 = text.index("Abstract")
+  elif "ABSTRACT" in text:
+    idx1 = text.index("ABSTRACT")
+  elif "Introduction" in text:
+    idx1 = text.index("Introduction")
+  elif "INTRODUCTION" in text:
+    idx1 = text.index("INTRODUCTION")
+  abstract = ''
+  for idx in range(0, idx1):
+    abstract = abstract + text[idx]
+  countries = []
+  for country in pycountry.countries:
+      if country.name in abstract or  country.alpha_3 in abstract:
+          countries.append(country.name)
+  if(len(countries)>0):
+    return countries.pop()
+  else:
+    return "Not Found"
+
+def tools(text):
+  techniques=''
+  tech=[]
+  tokens=nltk.word_tokenize(text)
+  for i in range(len(tokens)):
+    if (tokens[i]=="tools" or tokens[i]=="tool" ):
+      if (tokens[i-1]==")"):
+        tech.append(tokens[i-3])
+        tech.append(tokens[i-2])
+        tech.append(tokens[i-1])
+      else:
+        tech.append(tokens[i-1])
+      for j in range(i,i+10):
+        tech.append(tokens[j])
+      for k in range(i+11,len(tokens)):
+        tech.append(tokens[k])
+        if(tokens[k]=='.'):
+          break
+      tech.append("\n")
+  if(len(tech)>0):
+    return " ".join(tech)
+  else:
+    return "Not Found"
+
+def techniques(text):
+  techniques=''
+  tech=[]
+  tokens=nltk.word_tokenize(text)
+  for i in range(len(tokens)):
+    if (tokens[i]=="techniques" or tokens[i]=="technique" or tokens[i]=="technology" or tokens[i]=="technologies" or tokens[i]=="approach" or tokens[i]=="approaches"  ):
+      if (tokens[i-1]==")"):
+        tech.append(tokens[i-3])
+        tech.append(tokens[i-2])
+        tech.append(tokens[i-1])
+      else:
+        tech.append(tokens[i-1])
+      for j in range(i,i+15):
+        tech.append(tokens[j])
+      for k in range(i+16,len(tokens)):
+        tech.append(tokens[k])
+        if(tokens[k]=='.'):
+          break
+      tech.append("\n")
+    elif(tokens[i]=="references" or tokens[i]=="reference"):
+      break
+  if(len(tech)>0):
+    return " ".join(tech)
+  else:
+    return "Not Found"
+
+def problem(text):
+  if ("problem statement" in text):
+    idx1 = text.index("problem statement")
+    idx2 = idx1+500
+    future=''
+    for idx in range(idx1, idx2):
+      future = future + text[idx]
+    while(text[idx2]!='.'):
+      future = future + text[idx2]
+      idx2+=1
+    return future
+  elif ("introduction" in text):
+    idx1 = text.index("introduction")
+    idx2 = idx1+1000
+    future=''
+    for idx in range(idx1+len("introduction")+1, idx2):
+      future = future + text[idx]
+    while(text[idx2]!='.'):
+      future = future + text[idx2]
+      idx2+=1
+    return summary(future)
+  else:
+    problem = abstract(text)
+    return summary(problem)
+
+def problemdiscuss(text):
+  if ("problem" in text):
+    idx1 = text.index("problem")
+    idx2 = idx1+500
+    future=''
+    for idx in range(idx1, idx2):
+      future = future + text[idx]
+    while(text[idx2]!='.'):
+      future = future + text[idx2]
+      idx2+=1
+    return future
+  elif ("conclusion" in text):
+    idx1 = text.index("conclusion")
+    idx2 = idx1+1000
+    future=''
+    for idx in range(idx1+len("conclusion")+1, idx2):
+      future = future + text[idx]
+    while(text[idx2]!='.'):
+      future = future + text[idx2]
+      idx2+=1
+    return summary(future)
+  else:
+    problem = abstract(text)
+    return summary(problem)
+
+def futurework(text):
+  if ("future work" in text):
+    idx1 = text.index("future work")
+    idx2 = idx1+500
+    future=''
+    for idx in range(idx1, idx2):
+      future = future + text[idx]
+    while(text[idx2]!='.'):
+      future = future + text[idx2]
+      idx2+=1
+    return future 
+  elif ("future works" in text):
+    idx1 = text.index("future works")
+    idx2 = idx1+500
+    future=''
+    for idx in range(idx1, idx2):
+      future = future + text[idx]
+    while(text[idx2]!='.'):
+      future = future + text[idx2]
+      idx2+=1
+    return future
+  elif ("future direction" in text):
+    idx1 = text.index("future direction")
+    idx2 = idx1+500
+    future=''
+    for idx in range(idx1, idx2):
+      future = future + text[idx]
+    while(text[idx2]!='.'):
+      future = future + text[idx2]
+      idx2+=1
+    return future
+  elif ("future directions" in text):
+    idx1 = text.index("future directions")
+    idx2 = idx1+500
+    future=''
+    for idx in range(idx1, idx2):
+      future = future + text[idx]
+    while(text[idx2]!='.'):
+      future = future + text[idx2]
+      idx2+=1
+    return future
+  else:
+    return "Not Found"
 
 def getText(filename):
     if(".pdf" in filename):
@@ -120,32 +313,50 @@ def getText(filename):
           fullText.append(para.text)
       text = '\n'.join(fullText)
       text = re.sub(r'\s+', ' ',re.sub(r'\[[0-9]*\]', ' ', text))
+        
     dataframe = pd.DataFrame(columns=['Questions','Answers'])
-    newrow = {"Questions":"who is the author of the paper","Answers":author(text)}
+    newrow = {"Questions":"who is the author","Answers":author(text)}
+    dataframe = dataframe.append(newrow, ignore_index=True)
+    newrow = {"Questions":"what is the location of author","Answers":authorlocation(text)}
     dataframe = dataframe.append(newrow, ignore_index=True)
     text = text.lower()
-    newrow = {"Questions":"what is the summary of the paper","Answers":summary(text)}
+    newrow = {"Questions":"what is the summary","Answers":summary(text)}
     dataframe = dataframe.append(newrow, ignore_index=True)
-    newrow = {"Questions":"what is the abstract of the paper","Answers":abstract(text)}
+    newrow = {"Questions":"what is the abstract","Answers":abstract(text)}
+    dataframe = dataframe.append(newrow, ignore_index=True)
+    newrow = {"Questions":"what is the future work","Answers":futurework(text)}
+    dataframe = dataframe.append(newrow, ignore_index=True)
+    newrow = {"Questions":"what techniques has been discussed in paper","Answers":techniques(text)}
+    dataframe = dataframe.append(newrow, ignore_index=True)
+    newrow = {"Questions":"what technologies has been discussed in paper","Answers":techniques(text)}
+    dataframe = dataframe.append(newrow, ignore_index=True)
+    newrow = {"Questions":"what approaches has been discussed in paper","Answers":techniques(text)}
+    dataframe = dataframe.append(newrow, ignore_index=True)
+    newrow = {"Questions":"what tools has been suggested in paper","Answers":tools(text)}
+    dataframe = dataframe.append(newrow, ignore_index=True)
+    newrow = {"Questions":"what is the problem statement","Answers":problem(text)}
+    dataframe = dataframe.append(newrow, ignore_index=True)
+    newrow = {"Questions":"what is the problem discussed in the paper","Answers":problemdiscuss(text)}
     dataframe = dataframe.append(newrow, ignore_index=True)
     occurence,talking=talkabout(text)
     newrow = {"Questions":"what is the paper talking about","Answers":talking}
     dataframe = dataframe.append(newrow, ignore_index=True)
     occur = dict(occurence)
     for key,item in occur.items():
-      question = "How many times "+str(key)+" has been appeared"
-      answer = key +" has been appeared "+str(item)+" times"
-      newrow = {"Questions":question,"Answers":answer}
-      dataframe = dataframe.append(newrow, ignore_index=True)
+      if key!="direction" and key!="location" and key!="author" and key!="summary" and key!="talk" and key!="whose" and key!="abstract" and key!="summary" and key!="talking" and key!="problem" and key!="statement" and key!="tool" and key!="technology" and key!="approach" and key!="approach" and key!="future" and key!="work"  :
+        question = "How many times "+str(key)+" has been appeared"
+        answer = key +" has been appeared "+str(item)+" times"
+        newrow = {"Questions":question,"Answers":answer}
+        dataframe = dataframe.append(newrow, ignore_index=True)
     dataframe.to_csv("Rules.csv",index=False)
 
 def perform_lemmatization_stemming(tokens):
     tokens = [wnlemmatizer.lemmatize(token) for token in tokens]
     tokens = [ps.stem(token) for token in tokens]
+    stop_words = set(stopwords.words('english'))
+    tokens = [w for w in tokens if not w.lower() in stop_words]
     return tokens
-
 punctuation_removal = dict((ord(punctuation), None) for punctuation in string.punctuation)
-
 def get_processed_text(document):
     return perform_lemmatization_stemming(nltk.word_tokenize(document.lower().translate(punctuation_removal)))
 
@@ -204,3 +415,5 @@ def get_chatbotReply(question):
         return Chat_Bot_Answer(question,Chat_Bot_Responses)
     else:
         return answer
+
+getText("2206.04659.pdf")
