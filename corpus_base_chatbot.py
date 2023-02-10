c724e59eceb43d2122ded1207bf675f6b08b3fb7
usamarafique998@gmail.com <Osamarafique998>
2023-02-10 20:56:55 +0500

    Conclusion & Comments

--- corpus_base_chatbot.py
+++ corpus_base_chatbot.py
@@ -1,4 +1,5 @@
-import docx
+#Improting all important libraries requried for the work 
+import docx 
 import re
 import nltk
 from nltk.corpus import stopwords
@@ -41,6 +42,9 @@ from pattern.text.en import singularize
 ps = PorterStemmer()
 wnlemmatizer = nltk.stem.WordNetLemmatizer()
 
+
+#Summary function get the text and use the Text Rank Summarizer type of summary of SUMY Python Library
+#and get the summary of the text provide to save the summary of the paper 
 def summary(sample):
   parser = PlaintextParser.from_string(sample,Tokenizer("english"))
   summarizer = TextRankSummarizer()
@@ -50,6 +54,10 @@ def summary(sample):
       text_summary+=str(sentence)
   return text_summary
 
+
+#Abstract function find the abstract heading in the text and extract the text from the abstract to the introduction heading 
+#or keywords because normally at the end of abstract of the research paper there are some keywords mentioned. 
+#But if there are no keywords heading then it will extract text up to the introduction in the text and give the answer.
 def abstract(text):
   if ("abstract" in text):
     idx1 = text.index("abstract")
@@ -71,6 +79,12 @@ def abstract(text):
     abstract = abstract + text[idx]
   return abstract
 
+
+#Talk about function tokenize all the words in the text we extracted and then we use the counter library to get the 
+#count of all the words in the text excluding the stop words. We remove stop words from the text using nltk library 
+#and also, we use the techniques lemmatizing and stemming to get the base words and get the count and it will give 
+#two most frequently occurring words and then it will tell these two are the words paper is talking about and also it returns
+#list of all the cleaned tokens with their number of  occurence in the text
 def talkabout(text):
   notneeded=["technological","located","scope","research","outlook","techniques","technique","directions","direction","location" ,"author","talks","talk","whose","abstract","summary","talking","problem","statement","tools","tool","technologies","technology","approaches","approach","future","work","works"]
   data = text.translate(str.maketrans('', '', string.punctuation))
@@ -87,6 +101,12 @@ def talkabout(text):
   talking = "The paper is mostly talking about "+ most_occur[0][0]+" , "+most_occur[1][0]
   return word_counts,talking
 
+
+#Author function takes the text and extract the text from the beginning to the abstract heading because the author’s name is specially 
+#mention before the abstract. After extracting the text up to the abstract heading then we use the POS-Tagging and 
+#Name Entity Recognition technique of the NLP that tell us that these are the words whose entity is person and we will get 
+#the author name using this library. Some time author are more then one the code will check if there are more than one entity 
+#which are PERSON type then it will return more then one author names otherwise not found. 
 def author(text):
   idx1 = 0
   if "Abstract" in text:
@@ -125,6 +145,10 @@ def author(text):
     else:
       return "Not Found"
 
+
+#Author location function extract the text from the beginning to the abstract heading because the author location is specially mention 
+#before the abstract with the author names. After extracting text up to the abstract heading, we use the py country library that will 
+#look if any location is mentioned in the text we have and if it will have the location then it will be return as an answer of this question.
 def authorlocation(text):
   idx1 = 0
   if "Abstract" in text:
@@ -138,15 +162,19 @@ def authorlocation(text):
   abstract = ''
   for idx in range(0, idx1):
     abstract = abstract + text[idx]
+
   countries = []
   for country in pycountry.countries:
-      if country.name in abstract or  country.alpha_3 in abstract:
+      if country.name in abstract:
           countries.append(country.name)
   if(len(countries)>0):
     return countries.pop()
   else:
     return "Not Found"
 
+
+#Tools function find the tools heading in the text and extract the text from tools headings and if we don’t find tools headings from the text
+#it will then say Not Found otherwise it will give use the what tools are suggested in the paper?
 def tools(text):
   techniques=''
   tech=[]
@@ -171,6 +199,10 @@ def tools(text):
   else:
     return "Not Found"
 
+
+#Technique function find the technologies, approaches or technologies heading in the text and extract the text from these headings and if we don’t
+#find any of these headings from the text it will then say Not Found otherwise it will give use the what is the techniques, 
+#approaches or technologies discussed in the paper.
 def techniques(text):
   techniques=''
   tech=[]
@@ -197,6 +229,10 @@ def techniques(text):
   else:
     return "Not Found"
 
+
+#Problem function find the problem statement heading in the text and extract the text from problem statement headings and 
+#give the answer if we don’t find any problem statement headings from the text it will then give the summary of the introduction 
+#give in the research paper as mostly in the introduction, we describe the problem we are going to discuss in the paper
 def problem(text):
   if ("problem statement" in text):
     idx1 = text.index("problem statement")
@@ -222,6 +258,10 @@ def problem(text):
     problem = abstract(text)
     return summary(problem)
 
+
+#Problem Disccussed function find the problem statement heading in the text and extract the text from problem statement headings and 
+#give the answer if we don’t find any problem statement headings from the text it will then give the summary of the introduction 
+#give in the research paper as mostly in the introduction, we describe the problem we are going to discuss in the paper
 def problemdiscuss(text):
   if ("problem" in text):
     idx1 = text.index("problem")
@@ -247,6 +287,9 @@ def problemdiscuss(text):
     problem = abstract(text)
     return summary(problem)
 
+
+#Conclusion Function find the conclusion heading in the text and extract the text from conclusion headings upto the reffrence Heading
+#and give the answer if we don’t find any conclusion headings from the text it will then give say Not Found. 
 def conclusion(text):
   tokens=nltk.word_tokenize(text)
   word_counts = Counter(tokens)
@@ -255,12 +298,16 @@ def conclusion(text):
       tokens.remove("conclusion")
       word_counts = Counter(tokens)
   text=" ".join(tokens)
-  if ("conclusion" in text):
-    idx1 = text.index("conclusion")
-    if ("references" in text):
-      idx2 = text.index("references") 
-    elif("reference" in text):
-      idx2 = text.index("reference") 
+  if ("Conclusion" in text):
+    idx1 = text.index("Conclusion")
+    if ("References" in text):
+      idx2 = text.index("References") 
+    elif("Reference" in text):
+      idx2 = text.index("Reference")
+    elif("REFERENCE" in text):
+      idx2 = text.index("REFERENCE") 
+    elif("REFERENCES" in text):
+      idx2 = text.index("REFERENCES")  
     else:
       idx2 = idx1+1000
     future=''
@@ -270,10 +317,70 @@ def conclusion(text):
       future = future + text[idx2]
       idx2+=1
     return future
-  else:
-    
+  elif ("Conclusions" in text):
+    idx1 = text.index("Conclusions")
+    if ("References" in text):
+      idx2 = text.index("References") 
+    elif("Reference" in text):
+      idx2 = text.index("Reference") 
+    elif("REFERENCE" in text):
+      idx2 = text.index("REFERENCE") 
+    elif("REFERENCES" in text):
+      idx2 = text.index("REFERENCES")  
+    else:
+      idx2 = idx1+1000
+    future=''
+    for idx in range(idx1+len("conclusions")+1, idx2):
+      future = future + text[idx]
+    while(idx2<len(text) and text[idx2]!='.' ):
+      future = future + text[idx2]
+      idx2+=1
+    return future
+  elif ("CONCLUSION" in text):
+    idx1 = text.index("CONCLUSION")
+    if ("References" in text):
+      idx2 = text.index("References") 
+    elif("Reference" in text):
+      idx2 = text.index("Reference") 
+    elif("REFERENCE" in text):
+      idx2 = text.index("REFERENCE") 
+    elif("REFERENCES" in text):
+      idx2 = text.index("REFERENCES")  
+    else:
+      idx2 = idx1+1000
+    future=''
+    for idx in range(idx1+len("CONCLUSION")+1, idx2):
+      future = future + text[idx]
+    while(idx2<len(text) and text[idx2]!='.' ):
+      future = future + text[idx2]
+      idx2+=1
+    return future
+  elif ("CONCLUSIONS" in text):
+    idx1 = text.index("CONCLUSIONS")
+    if ("References" in text):
+      idx2 = text.index("References") 
+    elif("Reference" in text):
+      idx2 = text.index("Reference") 
+    elif("REFERENCE" in text):
+      idx2 = text.index("REFERENCE") 
+    elif("REFERENCES" in text):
+      idx2 = text.index("REFERENCES")  
+    else:
+      idx2 = idx1+1000
+    future=''
+    for idx in range(idx1+len("CONCLUSIONS")+1, idx2):
+      future = future + text[idx]
+    while(idx2<len(text) and text[idx2]!='.' ):
+      future = future + text[idx2]
+      idx2+=1
+    return future
+  else :
     return 'NOT FOUND'
 
+
+#Future work function find the future work or future direction heading in the text and extract the text from the future work 
+#and if it don’t find any future work or future direction heading from the text it will then say Not Found otherwise it will 
+#give use the what is the future works discussed in the paper
 def futurework(text):
   if ("future work" in text):
     idx1 = text.index("future work")
@@ -318,6 +425,13 @@ def futurework(text):
   else:
     return "Not Found"
 
+#Get Text Function takes the file name then the function will check if the given file is pdf file or word document file. 
+#If the file is in pdf form then the function will extract the text from the document using the py pdf reader library 
+#and if the given file is in the word document form then it will read the text from the file using the python docx library. 
+#Once the text is extracted now code will extract the information according to the questions we want in our chatbot we will
+#make a dataframe in which it will have question and answer question is mentioned manually and answer will extract by calling the
+#above functions according to the question for example if the question is who is the author the author function is called to get author name 
+#and save it in the dataframe. After all the question and answers are extarcted it will save the dataframe into a Rules.csv file
 def getText(filename):
     if(".pdf" in filename):
       pdfFileObj = open(filename, 'rb')
@@ -340,13 +454,16 @@ def getText(filename):
           fullText.append(para.text)
       text = '\n'.join(fullText)
       text = re.sub(r'\s+', ' ',re.sub(r'\[[0-9]*\]', ' ', text))
-        
+    
+    #Adding question and answer into the dataframes one by one
     dataframe = pd.DataFrame(columns=['Questions','Answers'])
     newrow = {"Questions":"who is the author","Answers":author(text)}
     dataframe = dataframe.append(newrow, ignore_index=True)
     newrow = {"Questions":"what is the location of author","Answers":authorlocation(text)}
     dataframe = dataframe.append(newrow, ignore_index=True)
     newrow = {"Questions":"what is the author location","Answers":authorlocation(text)}
+    dataframe = dataframe.append(newrow, ignore_index=True)
+    newrow = {"Questions":"what is the conclusion","Answers":conclusion(text)}
     dataframe = dataframe.append(newrow, ignore_index=True)
     text = text.lower()
     newrow = {"Questions":"what is the summary","Answers":summary(text)}
@@ -367,8 +484,6 @@ def getText(filename):
     dataframe = dataframe.append(newrow, ignore_index=True)
     newrow = {"Questions":"what is the problem discussed","Answers":problemdiscuss(text)}
     dataframe = dataframe.append(newrow, ignore_index=True)
-    newrow = {"Questions":"what is the conclusion","Answers":conclusion(text)}
-    dataframe = dataframe.append(newrow, ignore_index=True)
     occurence,talking=talkabout(text)
     newrow = {"Questions":"what is the paper talking about","Answers":talking}
     dataframe = dataframe.append(newrow, ignore_index=True)
@@ -379,18 +494,24 @@ def getText(filename):
         answer = key +" has been appeared "+str(item)+" times"
         newrow = {"Questions":question,"Answers":answer}
         dataframe = dataframe.append(newrow, ignore_index=True)
+    #Saving dataframe
     dataframe.to_csv("Rules.csv",index=False)
 
+#This function clean the data from the questions by removing stopwords, stemming words, and lemmitizing words
 def perform_lemmatization_stemming(tokens):
     tokens = [wnlemmatizer.lemmatize(token) for token in tokens]
     tokens = [ps.stem(token) for token in tokens]
     stop_words = set(stopwords.words('english'))
     tokens = [w for w in tokens if not w.lower() in stop_words]
     return tokens
+#Remove punctutaion
 punctuation_removal = dict((ord(punctuation), None) for punctuation in string.punctuation)
+#This function call the above perform lemmatization function by giving then the tokens of the text it receives
 def get_processed_text(document):
     return perform_lemmatization_stemming(nltk.word_tokenize(document.lower().translate(punctuation_removal)))
 
+
+#This Function give the answer of the question ask by the user
 def Chat_Bot_Answer(Question,Chat_Bot_Responses):
     #Append the question to the question defined earlier as rules
     Question_In_Rules=list(Chat_Bot_Responses.keys())
@@ -411,6 +532,8 @@ def Chat_Bot_Answer(Question,Chat_Bot_Responses):
     else: 
         return Chat_Bot_Responses[Closest_Question]
 
+
+#Get chatbot reply function read the rules and convert them into a dictionary and add the ask question to the dictionary to get response 
 def get_chatbotReply(question):
     question=question.lower()
     #Reading rules for chatbot from the CSV file
@@ -442,3 +565,4 @@ def get_chatbotReply(question):
     #Getting one to one response in present it in rules then will give answer
     #Will calculate cosine similarity with all questions and gives answer of the closest question if one to one question doesn't found
     return Chat_Bot_Answer(question,Chat_Bot_Responses)
+
--- main.py
+++ main.py
@@ -15,14 +15,20 @@ CORS(app, support_credentials=True)#import templates.chat_bot as chat
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 flag = True
 
+
+#Home API shows upload paper option when application is run
 @app.route('/')
 def home():
     return render_template('upload.html')
 
+#Index API shows chatbot interface when user upload the paper and submit it
 @app.route('/index')
 def upload():
     return render_template('index.html')
 
+#Chose CSV API is called when user submit the paper this api first of all move the uploaded file from its 
+#original directory to the current working directory and then it extract the text from the paper uploaded by calling the function
+#getText from the corpus base chatbot python file 
 @app.route('/chosecsv',methods=["POST"])
 def chosecsv():
     file = request.files['file']
@@ -33,6 +39,9 @@ def chosecsv():
     chats.getText(filename)
     return "Success"
 
+
+#Resposne API is called when user send a message to chatbot it will get chatbot response by calling the function
+#get_chatbotReply from the corpus base chatbot python file for the question user asked 
 @app.route('/response',methods=["POST"])
 def get_response():
     data = request.form['usermessage']
